5e5d52cd268b38ba4dcb2bc71af981cc80c0f6a7
Andres <andresg5997@gmail.com>
2019-10-30 22:38:48 -0500

    creadas funciones y condiciones de movimiento

--- js/index.js
+++ js/index.js
@@ -4,7 +4,6 @@ var listaDeNumeros = [[], [], [], []]
 var contador = 1
 
 for (var indexX = 0; indexX < 4; indexX++) {
-
   for (let indexY = 0; indexY < 4; indexY++) {
     numeroSimple = document.createElement('div')
     numeroSimple.className = 'numero'
@@ -14,3 +13,55 @@ for (var indexX = 0; indexX < 4; indexX++) {
     contador++
   }
 }
+
+function manejarPush(direccion) {
+  function verificarSiguienteNumero(numeroSeleccionado, numeroSiguiente) {
+    if(numeroSeleccionado === numeroSiguiente) {
+      unirNumeros()
+    }
+    else if(numeroSeleccionado !== numeroSiguiente) {
+      detenerMovimiento()
+    }
+    else if(numeroSiguiente === 0) {
+      moverNumero()
+    }
+
+  }
+
+  function unirNumeros() {
+
+  }
+
+  function detenerMovimiento() {
+
+  }
+
+  function moverNumero() {
+
+  }
+
+
+
+  function manejarmovimientos ( direccion ) { 
+    for (let direccionUno = 0; direccionUno < 4; direccionUno++) {
+      
+      for (let direccionDos = 0; direccionDos < 4; direccionDos++) {
+        if (direccionDos != 0) {
+
+          listaDeNumeros[direccionUno][direccionDos] = 
+        }
+      }
+      
+    }
+  }
+  switch (direccion) {
+    case 'ArrowUp':
+        
+      break;
+  }
+}
+
+document.addEventListener('keydown', function(event) {
+  console.log('event: ', event);
+
+})
